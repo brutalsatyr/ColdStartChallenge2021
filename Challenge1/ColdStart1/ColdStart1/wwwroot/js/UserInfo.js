f30c2dafce19db4f7e011cdb0f148c8210c1be28
brutalsatyr <39296986+brutalsatyr@users.noreply.github.com>
2021-03-04 18:59:39 +0100

    Test out authentication on azure

---
+++ Challenge1/ColdStart1/ColdStart1/wwwroot/js/UserInfo.js
@@ -0,0 +1,12 @@
+﻿async function getUserInfo() {
+    try {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        console.log(clientPrincipal);
+        return clientPrincipal;
+    } catch (error) {
+        console.error('No profile could be found');
+        return undefined;
+    }
+}
